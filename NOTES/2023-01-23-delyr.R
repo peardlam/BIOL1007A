e7badace56ee0c924fdf50688cc6631fd4f1c051
Pearl Langelihle Dlamini <pearllangelihledlamini@pearls-air-2.middlebury.edu>
2023-01-23 16:09:09 -0500

    getting webpage up

---
+++ NOTES/2023-01-23-delyr.R
@@ -0,0 +1,22 @@
+### Entering the tidyverse
+### 23 January 2023
+### LVA
+
+### tidyverse: collection of "opinionated" packages that share philosophy, grammar (or how the code is structured), and data structures
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
